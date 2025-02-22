6a60c175b535e6769eb664f4f22bd25626744880
Syed Tasavour <syedtasavour86@gmail.com>
2025-02-22 16:48:05 +0000

    add test.js with function to evaluate parameters

---
+++ test.js
@@ -0,0 +1,22 @@
+
+
+function name(params) {
+    if (params) {
+        console.log(true);
+        
+    }else{
+        console.log(false);
+        
+    }
+}
+
+name(1)
+name(2)
+name("")
+name("test")
+name([1,343])
+name([])
+name({})
+name({
+    test: "ebfhbe"
+})
